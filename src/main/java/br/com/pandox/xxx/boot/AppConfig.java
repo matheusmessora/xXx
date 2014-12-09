ec45b47aa683adfdd109dfeaf4b961d06ae28ead
Matheus Messora <tqi_mmessora@uolinc.com>
2014-12-09 19:43:10 -0200

    pray. RESTfull

--- src/main/java/br/com/pandox/xxx/boot/AppConfig.java
+++ src/main/java/br/com/pandox/xxx/boot/AppConfig.java
@@ -29,7 +29,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import java.util.Properties;
 
 /**
- * Classe de configuração Spring MVC 3
+ * Classe de configuração Spring MVC
  */
 @Configuration
 @PropertySource("classpath:config.properties")
--- src/main/java/br/com/pandox/xxx/controller/view/IndexController.java
+++ src/main/java/br/com/pandox/xxx/controller/view/IndexController.java
@@ -1,9 +1,7 @@
 package br.com.pandox.xxx.controller.view;
 
 import br.com.pandox.xxx.model.nf.Invoice;
-import br.com.pandox.xxx.service.AsyncService;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,10 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     private static Logger log = Logger.getLogger(IndexController.class);
-
-    @Autowired
-    private AsyncService service;
-
 
     @RequestMapping("/")
     public Invoice index() {
